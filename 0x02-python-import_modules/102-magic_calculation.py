a2aacee3ad5fe27ed20e0ac67b19a9876132b124
ghita-as <ghita.as.amranisouhli@gmail.com>
2024-01-04 05:47:43 -0800

    0x02. Python - import & modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102',
-            fromlist=['add', 'sub']).add, __import__('magic_calculation_102',
-                    fromlist=['add', 'sub']).sub
-    
+    # Dummy import for bytecode compatibility
+    import magic_calculation_102
+
+    # Import add and sub functions
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+
+    # Check if a is less than b
     if a < b:
+        # Calculate c by adding a and b
         c = add(a, b)
-        
+
+        # Iterate over the range (4, 6) and add to c
         for i in range(4, 6):
             c = add(c, i)
-        
+
         return c
     else:
+        # If a is not less than b, perform sub(a, b)
         return sub(a, b)
