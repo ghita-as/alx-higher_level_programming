e0a893c551e53de729100e602792c4d6c9e3e314
ghita-as <ghita.as.amranisouhli@gmail.com>
2024-01-05 23:53:44 -0800

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 
 /**
- *add_nodeint - adds a new node at the beginning of a listint_t list
- *@head: head of listint_t
- *@n: int to add in listint_t list
- *Return: address of the new element, or NULL if it failed
- */
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
@@ -21,10 +21,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	return (new);
 }
 /**
- *is_palindrome - identify if a syngle linked list is palindrome
- *@head: head of listint_t
- *Return: 1 if it is palindrome else 0
- */
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
 int is_palindrome(listint_t **head)
 {
 	listint_t *head2 = *head;
