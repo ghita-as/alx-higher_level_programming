c17720b2f58ea7ca20296352356520c03f9111fe
ghita-as <ghita.as.amranisouhli@gmail.com>
2024-01-07 11:29:32 -0800

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,53 +1,36 @@
-#include "lists.h"
-#include <stdlib.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	listint_t *new;
+	listint_t *head;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-/**
-*is_palindrome - identify if a syngle linked list is palindrome
-*@head: head of listint_t
-*Return: 1 if it is palindrome else 0
-*/
-int is_palindrome(listint_t **head)
-{
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
+	head = NULL;
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 1);
+	print_listint(head);
 
-	if (*head == NULL || head2->next == NULL)
-		return (1);
-	while (head2 != NULL)
-	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
-	}
-	aux2 = aux;
-	while (*head != NULL)
-	{
-		if ((*head)->n != aux2->n)
-		{
-			free_listint(aux);
-			return (0);
-		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
-	}
-	free_listint(aux);
-	return (1);
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	return (0);
 }
