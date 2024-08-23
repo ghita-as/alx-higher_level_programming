a045dad9d566e7fc1dee8829b56f608ceb836e75
ghita-as <ghita.as.amranisouhli@gmail.com>
2024-08-23 13:41:26 +0000

    0x0B. Python

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 
-"""Defines a class Student."""
+"""Defines a Student class"""
+
 
 class Student:
-    """Represent a student."""
+    """A Student class"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """Initialize a Student object
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list. Otherwise, all attributes are represented.
+        """Retrieves a dictionary representation of a Student
+        If attrs is a list of strings, only attribute names
+        contained in this list are retrieved.
         Args:
-            attrs (list): (Optional) The attributes to represent.
+            attrs (list): attributes to retrieve
         """
-        if attrs is None:
-            return self.__dict__
-        if all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
-        return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        if type(attrs) == list:
+            if all(type(elem) == str for elem in attrs):
+                return {key: getattr(self, key)
+                        for key in attrs if hasattr(self, key)}
+        return self.__dict__
